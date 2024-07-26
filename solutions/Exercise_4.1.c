d5781066672ec1125614d5ef1123022ceb42d79b
accehikllmr <135480854+accehikllmr@users.noreply.github.com>
2024-07-26 13:07:38 -0400

    Add files via upload

---
+++ solutions/Exercise_4.1.c
@@ -0,0 +1,31 @@
+//need to separate lines into paragraphs (paragraphing)
+#include <stdio.h>  //paragraph for importing libraries
+#include <math.h>
+
+//paragraph for function declaration and body
+//K&R style for placement of braces (curly brackets)
+int main(void) {     //white space prior to curly bracket
+                     //best practice to use int main(void) because we want main to be called without arguments
+                     //(not writing void allows main to be called with arguments)
+
+//paragraph for variable declaration
+    double sMajAxis, sMinAxis, ec;     //white space before variable after comma
+                                       //give variables meaningful names
+
+//paragraphs for taking user inputs
+    printf("Semi-major axis :");     //white space after colon
+    scanf("%lf", &sMajAxis);
+
+    printf("Semi-minor axis :");
+    scanf("%lf", &sMinAxis);
+
+//paragraph for conditional statement
+//K&R style for placement of braces (curly brackets)
+    if (sMajAxis >= sMinAxis) {
+        ec = sqrt(sMajAxis * sMajAxis - sMinAxis * sMinAxis) / sMajAxis;    //white spaces between variables and operators, parentheses and curly brackets
+        printf("E = %.5lf", ec);                                            //and if and parenthesis
+
+    }
+//paragraph for return statement
+    return 0;
+}
