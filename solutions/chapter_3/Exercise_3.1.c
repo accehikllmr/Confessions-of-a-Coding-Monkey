c39cf19d3ef8732a1fe47bc4b96a2f41acfc2f6c
accehikllmr <135480854+accehikllmr@users.noreply.github.com>
2024-07-26 13:10:49 -0400

    Update Exercise_3.1.c

--- solutions/chapter_3/Exercise_3.1.c
+++ solutions/chapter_3/Exercise_3.1.c
@@ -9,14 +9,14 @@ int main() {
     float P;
 
     printf("Efficiency: ");
-    scanf("%f", &efficiency);    //DO NOT use next line escape character after format specifier, not necessary and will ask for second input before continuing
+    scanf("%f", &efficiency); //DO NOT use next line escape character after format specifier, not necessary and will ask for second input before continuing
     printf("Water Flow: ");
     scanf("%f", &waterFlow);
     printf("Water Head: ");
     scanf("%f", &waterHead);
 
     P = efficiency * density * gravity * waterHead * waterFlow;
-    printf("The power generated is %.0f kilowatts.", P);            //use .0f to return a value without decimal figures
+    printf("The power generated is %.0f kilowatts.", P); //use .0f to return a value without decimal figures
 
     return 0;
 }
