65e59fb2fab6243ea1900bb85eb207e04ef92f6c
accehikllmr <135480854+accehikllmr@users.noreply.github.com>
2024-07-29 20:38:09 -0400

    Add files via upload

---
+++ solutions/chapter_3/Exercise_3.4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    //declaring all variables at once, according to their common types (enter integers as floats to avoid calculation problems)
+    float newsUse, massUse, energyUse, tonUse, volumeUse, treeUse;
+    const float NEWSMASS = 0.3, NEWSENERGY = 61, TONKG = 0.001, WOODFIBRE = 1.8, TREEVOLUME = 0.25;
+
+    printf("Newspaper used per week: ");
+    scanf("%f", &newsUse);
+    //only known strategy to skip one line, since inserting into scanf function causes asking for double input
+    printf("\n");
+
+    //calculations to determine the amount of energy used the given mass of newspapers
+    massUse = newsUse * NEWSMASS;
+    energyUse = massUse * NEWSENERGY;
+
+    //calculation to determine the number of trees used to make the given mass of newspapers
+    tonUse = massUse * TONKG;
+    volumeUse = tonUse * WOODFIBRE;
+    treeUse = ceil(volumeUse / TREEVOLUME);
+
+    printf("Approximately %0.f Megajoules of energy and %0.f whole trees were used to produce %0.f kilograms of newspaper.\n", energyUse, treeUse, massUse);
+
+    return 0;
+}
