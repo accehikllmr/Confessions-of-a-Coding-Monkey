b3eb4092fd955da712e9b5ac8bf67af2f87b9518
accehikllmr <135480854+accehikllmr@users.noreply.github.com>
2024-07-31 14:24:08 -0400

    Add files via upload

---
+++ solutions/chapter_4/Exercise_5.1_assertions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//library needed to use assert macro (what is a macro?)
+#include <assert.h>
+
+int main(void)
+{
+    int num, den, mod;
+    scanf("%d%d", &num, &den);
+
+    //assertion which, if failed, will abort the program thus preventing the runtime error that would follow (division by zero)
+    //also writes an error message to standard output
+    assert(den > 0);
+
+    mod = num % den;
+    printf("remainder = %d", mod);
+}
